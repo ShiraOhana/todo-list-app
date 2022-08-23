beb8898517c31e2a54f0a5e90192ecc43eb18278
ShiraOhana <soshiraohana@gmail.com>
2022-08-23 11:32:06 +0300

    files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import "dotenv/config";
 
 const app = express();
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
@@ -9,18 +10,13 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://shira:Lucky123@cluster0.rwjlm9x.mongodb.net/?retryWrites=true&w=majority";
+const URI = process.env.ATLAS_URI;
 const PORT = process.env.PORT || 5050;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server is up and running on port ${PORT}`)
-    )
-  )
-  .catch((err) => console.log(err));
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -28,3 +24,5 @@ app.get("/users", (req, res) => {
   console.log(req.body);
   res.send("testing");
 });
+
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
